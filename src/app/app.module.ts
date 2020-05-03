ee1a65e215892a3d6e5c1985a9cc20d3f6030b96
selenatuyen <selenaltuyen@gmail.com>
2020-05-03 16:34:50 -0700

    Remove navigation files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { NavigationComponent } from './navigation/navigation.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
@@ -15,7 +14,6 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     ShoppingListComponent,
-    NavigationComponent,
     RecipeComponent,
     RecipeListComponent,
     RecipeItemComponent,
